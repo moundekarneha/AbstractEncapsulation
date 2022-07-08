a3bbfbd15b3f2a528fed5732df396b1a1d8b9798
HP <HP@DESKTOP-G4J7E8F>
2022-07-08 17:07:56 +0530

    Add project files.

---
+++ AbstractEncapsulation/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractEncapsulation
+{
+
+
+    /// <summary>
+    /// Abstraction - what is needed = user only need custcode and cust name 
+    /// Encapsulation - made all other code private - achive through access modifiers =validate is private since customer not needed this
+    /// </summary>
+    internal class Program
+    {
+        class Customer
+        {
+            public string custCode { get; set; }    
+            public string custName { get; set; }
+
+            public void Add()
+            {
+                Validate();
+            }
+            private void Validate()
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Customer obj = new Customer();
+            obj.custCode = "000";
+
+            obj.custName = "Neha";
+            obj.Add();
+
+        }
+    }
+}
